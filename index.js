01bd91508edaed3d4be0b9805bc3fdb14ddf21c5
markusvankempen <markusvankempen43@gmail.com>
2019-09-05 16:30:52 -0400

    mvk

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ var server = http.createServer(function(request, response) {
 
 });
 
-var port = 8080;
-server.listen(port);
-
-console.log("Server running at http://localhost:%d", port);
+var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+server.listen( port, ipaddress, function() {
+    console.log((new Date()) + ' Server is listening on port %d', port);
+});
