043924bb8a75efd8f4e22f9ee849ff64e9e5e642
markusvankempen <markusvankempen43@gmail.com>
2019-09-05 16:46:19 -0400

    asdsad

--- index.js
+++ index.js
@@ -7,7 +7,8 @@ var server = http.createServer(function(request, response) {
     response.end("Markus Test \n Hello World! ");
 });
 
-var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+var ipaddress =  process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
+
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 server.listen( port, ipaddress, function() {
     console.log((new Date()) + ' %s Server is listening on port %d', ipaddress,port);
