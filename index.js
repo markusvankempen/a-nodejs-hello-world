06049c7b5aed055a2f918b0f553af039531456ee
markusvankempen <markusvankempen43@gmail.com>
2019-09-05 16:43:26 -0400

    amvk

--- index.js
+++ index.js
@@ -5,11 +5,10 @@ var server = http.createServer(function(request, response) {
     response.writeHead(200, {"Content-Type": "text/plain"});
       response.write("a-nodejs-hello-world\n")
     response.end("Markus Test \n Hello World! ");
-
 });
 
 var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
 server.listen( port, ipaddress, function() {
-    console.log((new Date()) + ' Server is listening on port %d', port);
+    console.log((new Date()) + ' %s Server is listening on port %d', ipaddress,port);
 });
